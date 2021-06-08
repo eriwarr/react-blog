480755435a7a7df627ca6902f20ad92b8bb0f48b
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-06-07 21:17:56 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: '',
+      body: '',
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.savePost = this.savePost.bind(this);
+  }
+
+handleInput(event) {
+this.setState({ [event.target.name]: event.target.value });
+
+}
+
+savePost(event) {
+  event.preventDefault();
+  console.log(this.state)
+  this.setState({ title: '', body: ''})
+}
+
+  render() {
+    return (
+      <>
+      <form onSubmit={this.savePost}>
+        <input type="text" name="title" placeholder="Blog's Title" value={this.state.title}onChange={this.handleInput}/>
+        <input className="form-body" name="body" value={this.state.body} type="text"onChange={this.handleInput}/>
+        <button type="submit">Save Blog Post</button>
+      </form>
+
+      </>
+    )
+  }
 }
 
 export default App;
